a8a5829419093cffb8558b2e1f3100aa5c7638df
Danniell Hu <dannihu01@gmail.com>
2018-12-11 19:28:24 -0500

    Elevator init

---
+++ Subsystems/elevator.py
@@ -0,0 +1,15 @@
+import wpilib 
+
+class Elevator:
+	""" Elevator is lifting the grabber mechanism."""
+	def __init__(self, motor):
+		self.motor = motor
+
+	def go_up(self, speed = 1.0):
+		self.motor.set(speed)
+
+	def go_down(self, speed = 1.0):
+		self.motor.set(-speed)
+
+	def stop(self):
+		self.motor.set(0)
